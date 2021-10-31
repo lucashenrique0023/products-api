0bb3a83e296245d5ee0cf708e6d464b6954f5406
Lucas Henrique <lucas.henrique0023@gmail.com>
2021-10-31 18:14:27 -0300

    [DEV] Product Representation Class
    - Initial development of Product representation class.
    - Changes on ProductsController to work with Product instead Strings

--- src/products.controller.ts
+++ src/products.controller.ts
@@ -1,32 +1,38 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { Product } from "./product.model";
 
 @Controller('products')
 export class ProductsController {
 
+    products: Product[] = [
+        new Product("BOOK01", "Book 01", 29.90),
+        new Product("BOOK02", "Book 02", 32.90),
+        new Product("BOOK03", "Book 03", 33.33)
+    ]
+
     @Get()
-    getAll(): String {
-        return 'List of all products';
+    getAll(): Product[] {
+        return this.products;
     }
 
     @Get(':id')
-    getProduct(@Param() params): String {
-        return `Return product data ${params.id}`;
+    getProduct(@Param() params): Product {
+        return this.products[0];
     }
 
     @Post()
-    create(@Body() product): String {
-        console.log(product);
-        return 'Product created.';
+    create(@Body() product: Product) {
+        product.id = 100;
+        this.products.push(product);
     }
 
     @Put()
-    edit(@Body() product): String {
-        console.log(product);
-        return 'Product Modified.';
+    edit(@Body() product: Product): Product {
+        return product;
     }
 
     @Delete(':id')
-    delete(@Param() params): String {
-        return `Product ${params.id} removed.`
+    delete(@Param() params) {
+        this.products.pop();
     }
 }
